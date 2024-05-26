b097adaa2cdd6f94d03507b0f02af4acbbe1628b
bichdao090202 <bichdao090202@gmail.com>
2024-05-27 00:30:49 +0700

    save git ignore

---
+++ src/main/java/vn/edu/creditbasedcourseregistrationsystem/CreditBasedCourseRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.creditbasedcourseregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditBasedCourseRegistrationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditBasedCourseRegistrationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/creditbasedcourseregistrationsystem/CreditBasedCourseRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.creditbasedcourseregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditBasedCourseRegistrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
